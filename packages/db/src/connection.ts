5504baf4b61a80dd1266e2f07958e82f544db255
ben <cowleyben23@gmail.com>
2025-12-22 05:50:41 +0800

    db connection fix

--- packages/db/src/connection.ts
+++ packages/db/src/connection.ts
@@ -1,4 +1,12 @@
+import { createPool, sql } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
+
+import * as schema from './schema';
 import { env } from '@repo/env';
 
-export const db = drizzle(env.DATABASE_URL);
+export const db = drizzle({
+  client: createPool({
+    connectionString: env.DATABASE_URL,
+  }),
+  schema,
+});
