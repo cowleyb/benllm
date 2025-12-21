cfcef7abf223554b712f4077b1487398da66b33a
ben <cowleyben23@gmail.com>
2025-12-21 23:31:34 +0800

    add schema

---
+++ packages/db/src/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, uuid, varchar, text, timestamp, foreignKey, unique, boolean } from 'drizzle-orm/pg-core';
+
+export const post = pgTable('post', {
+  id: uuid().defaultRandom().primaryKey().notNull(),
+  title: varchar({ length: 256 }).notNull(),
+  content: text().notNull(),
+  createdAt: timestamp('created_at', { mode: 'string' }).defaultNow().notNull(),
+  updatedAt: timestamp('updated_at', { mode: 'string' }),
+});
+
+export * from './auth-schema';
