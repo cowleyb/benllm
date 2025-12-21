7e12c178ec183ad8593c5def163d838181271faf
ben <cowleyben23@gmail.com>
2025-12-22 02:15:59 +0800

    auth and env packages

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,17 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { auth } from '@repo/auth';
+import { cors } from 'hono/cors';
 
-const app = new Hono()
+const app = new Hono();
 
-const welcomeStrings = [
-  'Hello Hono!',
-  'To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono'
-]
+const welcomeStrings = ['Hello Hono!', 'To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono'];
 
 app.get('/', (c) => {
-  return c.text(welcomeStrings.join('\n\n'))
-})
+  return c.text(welcomeStrings.join('\n\n'));
+});
 
-export default app
+app.on(['POST', 'GET'], '/api/auth/*', (c) => {
+  return auth.handler(c.req.raw);
+});
+
+export default app;
---
+++ packages/auth/src/auth.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+
+import { db } from '@repo/db';
+import { env } from '@repo/env';
+
+const baseUrl = env.VERCEL_ENV === 'production' ? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}` : env.VERCEL_ENV === 'preview' ? `https://${env.VERCEL_URL}` : 'http://localhost:5173';
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg', // or "mysql", "sqlite"
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    discord: {
+      clientId: env.BETTER_AUTH_DISCORD_CLIENT_ID,
+      clientSecret: env.BETTER_AUTH_DISCORD_CLIENT_SECRET,
+      redirectURI: `${baseUrl}/api/auth/callback/discord`,
+    },
+  },
+});
