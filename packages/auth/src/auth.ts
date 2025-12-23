1fa215313590c0b293ef178db42f90cd8ff28150
ben <cowleyben23@gmail.com>
2025-12-24 07:51:18 +0800

    auth redirect changed

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -4,9 +4,10 @@ import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '@repo/db';
 import { env } from '@repo/env';
 
-const baseUrl = env.VERCEL_ENV === 'production' ? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}` : env.VERCEL_ENV === 'preview' ? `https://${env.VERCEL_URL}` : 'http://localhost:5173';
+// const baseUrl = env.VERCEL_ENV === 'production' ? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}` : env.VERCEL_ENV === 'preview' ? `https://${env.VERCEL_URL}` : 'http://localhost:5173';
+// console.log(env.VERCEL_ENV, baseUrl);
 
-console.log(env.VERCEL_ENV, baseUrl);
+const baseUrl = env.VERCEL_ENV == 'production' ? `https://chat.bencow.dev` : 'http://localhost:5173';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
